8fccfeec39691ae7581d5f85f63c889790328c8a
Joost <joostvherwaarden@gmail.com>
2016-11-16 11:28:50 +0100

    Tutorial completed + added some stuff on my own to make the game more enjoyable

--- UnityNetworking/Assets/Scripts/Health.cs
+++ UnityNetworking/Assets/Scripts/Health.cs
@@ -1,33 +1,71 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Collections;
 
-public class Health : NetworkBehaviour 
-{
+public class Health : NetworkBehaviour {
+
 	public const int maxHealth = 100;
+	public bool destroyOnDeath;
 
 	[SyncVar(hook = "OnChangeHealth")]
 	public int currentHealth = maxHealth;
 
 	public RectTransform healthBar;
 
-	public void TakeDamage(int amount)
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start ()
 	{
-		if (!isServer)
+		if (isLocalPlayer)
 		{
-			return;
-		}
-		
-		currentHealth -= amount;
-		if (currentHealth <= 0)
-		{
-			currentHealth = 0;
-			Debug.Log("Dead!");
+			spawnPoints = FindObjectsOfType<NetworkStartPosition>();
 		}
 	}
 
-	void OnChangeHealth (int health)
+	public void TakeDamage(int amount)
 	{
-		healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+		if (!isServer)
+			return;
+
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			if (destroyOnDeath)
+			{
+				Destroy(gameObject);
+			} 
+			else
+			{
+				currentHealth = maxHealth;
+
+				// called on the Server, invoked on the Clients
+				RpcRespawn();
+			}
+		}
+	}
+
+	void OnChangeHealth (int currentHealth )
+	{
+		healthBar.sizeDelta = new Vector2(currentHealth , healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			// Set the spawn point to origin as a default value
+			Vector3 spawnPoint = Vector3.zero;
+
+			// If there is a spawn point array and the array is not empty, pick one at random
+			if (spawnPoints != null && spawnPoints.Length > 0)
+			{
+				spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+			}
+
+			// Set the player’s position to the chosen spawn point
+			transform.position = spawnPoint;
+		}
 	}
 }
